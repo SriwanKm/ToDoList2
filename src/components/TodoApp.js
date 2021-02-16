7c52f27441577c30a04cf984520ffdc6d7a94aea
SriwanKm <kcqkoo@gmail.com>
2021-02-16 15:47:24 -0700

    added task, list, delete, and completed buttons

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,82 +1,47 @@
-import React, { useState } from "react";
-import "./todoapp.css";
+import React, {useState} from "react";
+import "./todoapp.css"
 
 function TodoApp() {
-  const [task, setTask] = useState("");
-  const [tasklist, setTaskList] = useState([]);
 
-  const handleChange = (e) => {
-    setTask(e.target.value);
-  };
-
-  const AddTask = () => {
-    if (task !== "") {
-      const taskDetails = {
-        id: Math.floor(Math.random() * 1000),
-        value: task,
-        isCompleted: false,
-      };
-
-      setTaskList([...tasklist, taskDetails]);
+    const [task, setTask] = useState("");
+    const handleChange = (e) => {
+        setTask(e.target.value)
     }
-  };
+    const [taskList, setTaskList] = useState([])
+    const addTask = () => {
+        if (task !== "") {
+            const taskDetails = {
+                id: Math.floor(Math.random() * 1000),
+                value: task,
+                isCompleted: false
+            }
+            setTaskList([...taskList, taskDetails])
+        }
+    }
 
-  const deletetask = (e, id) => {
-    e.preventDefault();
-    setTaskList(tasklist.filter((t) => t.id != id));
-  };
 
-  const taskCompleted = (e, id) => {
-    e.preventDefault();
-    //let's find index of element
-    const element = tasklist.findIndex((elem) => elem.id == id);
+    // const handleList = () => taskList !== [] ? taskList.map(t => "<li>" + t.value + "</li>") : null
 
-    //copy array into new variable
-    const newTaskList = [...tasklist];
+    // console.log("taskList", taskList);
+    // const list = taskList ? taskList.map(t => "<li className='listitem'>" + t.value + "</li>") : null
 
-    //edit our element
-    newTaskList[element] = {
-      ...newTaskList[element],
-      isCompleted: true,
-    };
-
-    setTaskList(newTaskList);
-  };
-
-  return (
-    <div className="todo">
-      <input
-        type="text"
-        name="text"
-        id="text"
-        onChange={(e) => handleChange(e)}
-        placeholder="Add task here..."
-      />
-      <button className="add-btn" onClick={AddTask}>
-        Add
-      </button>
-      <br />
-      {tasklist !== [] ? (
+    const makeList = () => taskList !== [] ?
         <ul>
-          {tasklist.map((t) => (
-            <li className={t.isCompleted ? "crossText" : "listitem"}>
-              {t.value}
-              <button
-                className="completed"
-                onClick={(e) => taskCompleted(e, t.id)}
-              >
-                Completed
-              </button>
-
-              <button className="delete" onClick={(e) => deletetask(e, t.id)}>
-                Delete
-              </button>
-            </li>
-          ))}
+            {taskList.map(t => <li className="listitem">{t.value}
+                <button className="delete">Delete</button>
+                <button className="completed">Completed</button>
+            </li>)}
         </ul>
-      ) : null}
-    </div>
-  );
+        : null;
+
+    return (
+        <div className="todo">
+            <input onChange={(e) => handleChange(e)} type="text" name="text" id="text"
+                   placeholder="Add task here..."/>
+            <button className="add-btn" onClick={addTask}>Add</button>
+            {makeList()}
+        </div>
+    )
 }
 
-export default TodoApp;
+export default TodoApp
