fcd84a498f712a138c66c91dcc4b9167f35ecab0
SriwanKm <kcqkoo@gmail.com>
2021-02-18 17:00:13 -0700

    Updates

---
+++ precache-manifest.512f47a5afda4e37353c2fd298e802f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "104d7d4af2a1b5f2051ba584cedd5f19",
+    "url": "/ToDoList2/index.html"
+  },
+  {
+    "revision": "36a5be5925c7f0d8c457",
+    "url": "/ToDoList2/static/css/main.ac60c21d.chunk.css"
+  },
+  {
+    "revision": "3b42d42cda4e787b4b3c",
+    "url": "/ToDoList2/static/js/2.c5cd6c3b.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/ToDoList2/static/js/2.c5cd6c3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36a5be5925c7f0d8c457",
+    "url": "/ToDoList2/static/js/main.a368c7a8.chunk.js"
+  },
+  {
+    "revision": "30fb32fa40863ba6ab8c",
+    "url": "/ToDoList2/static/js/runtime-main.3e4cda84.js"
+  },
+  {
+    "revision": "4f646371b2a2501c7acd9110625993ca",
+    "url": "/ToDoList2/static/media/background.4f646371.jpg"
+  }
+]);
